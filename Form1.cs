f4f3709b02f92f4d18b647056182d3c7582fe625
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-11-13 11:26:09 -0500

    kh

--- Form1.cs
+++ Form1.cs
@@ -14,10 +14,13 @@ namespace Greating_card
     {
         Pen darkturquoisepen = new Pen(Color.DarkTurquoise, 5);
         Pen greenpen = new Pen(Color.Green, 5);
-        Pen blackpen = new Pen(Color.Black, 5);
+        Pen blackpen = new Pen(Color.Black);
         Pen turquoisepen = new Pen(Color.Turquoise, 5);
         Pen Whitepen = new Pen(Color.White, 5);
         Pen deepskybluepen = new Pen(Color.DeepSkyBlue, 5);
+        Pen orangepen = new Pen(Color.Orange);
+        Pen redpen = new Pen(Color.Red);
+
 
         SolidBrush blackbrush = new SolidBrush(Color.Black);
         SolidBrush redbrush = new SolidBrush(Color.Red);
@@ -25,6 +28,8 @@ namespace Greating_card
         SolidBrush whitebrush = new SolidBrush(Color.White);
         SolidBrush turquoisebrush = new SolidBrush(Color.Turquoise);
         SolidBrush deepskybluebrush = new SolidBrush(Color.DeepSkyBlue);
+        SolidBrush orangebrush = new SolidBrush(Color.Orange);
+       
         public Form1()
         {
             InitializeComponent();
@@ -38,6 +43,7 @@ namespace Greating_card
 
             g.DrawLine(Whitepen, 40, 40, 100, 100);
             g.DrawLine(Whitepen, 40, 100, 100, 40);
+            g.DrawLine(Whitepen, 70, 40, 70, 100);
 
            
             Font drawFont = new Font("flame", 20, FontStyle.Bold);
@@ -55,8 +61,46 @@ namespace Greating_card
             g.DrawString("LET'S BUILD A \n SNOWMAN", drawFont, deepskybluebrush, 100, 100);
 
             g.DrawEllipse(Whitepen, 25, 200, 100, 100);
+            g.DrawEllipse(Whitepen, 25, 127, 75, 75);
+            g.DrawEllipse(Whitepen, 25, 77, 50, 50);
+            g.FillEllipse(whitebrush, 25, 200, 100, 100);
+            g.FillEllipse(whitebrush, 25, 127, 75, 75);
+            g.FillEllipse(whitebrush, 25, 77, 50, 50);
+
+            g.DrawPie(orangepen, 50, 100, 50, 50, 210, 30);
+            g.FillPie(orangebrush, 50, 100, 50,50,210,30);
+
+            g.DrawEllipse(blackpen, 55, 95 , 5, 5);
+            g.DrawEllipse(blackpen, 44, 100, 5, 5);
+            g.FillEllipse(blackbrush, 55, 95, 5, 5);
+            g.FillEllipse(blackbrush, 44, 100, 5, 5);
+
+            g.DrawPie(redpen, -25, 6, 90, 90, 30, 60);
+            g.FillPie(redbrush,-25, 6, 90, 90, 30, 60);
+            g.FillEllipse(blackbrush, 10,45, 20, 20);
+
+            int i = 0; 
+
+            g.DrawEllipse(Whitepen, 55, 310, 5, 5);
+            g.DrawEllipse(Whitepen, 130, 95, 5, 5);
+            g.DrawEllipse(Whitepen, 100, 120, 5, 5);
+            g.DrawEllipse(Whitepen, 200, 300, 5, 5);
+            g.DrawEllipse(Whitepen, 250, 120, 5, 5);
+            g.DrawEllipse(Whitepen, 150, 95, 5, 5);
+            g.DrawEllipse(Whitepen, 300, 200, 5, 5);
+            g.DrawEllipse(Whitepen, 200, 80, 5, 5);
+            g.DrawEllipse(Whitepen, 100, 300, 5, 5);
+            g.DrawEllipse(Whitepen, 100, 150, 5, 5);
+            g.DrawEllipse(Whitepen, 160, 260, 5, 5);
+            g.DrawEllipse(Whitepen, 200, 200, 5, 5);
+            g.DrawEllipse(Whitepen, 100, 60, 5, 5);
+            g.DrawEllipse(Whitepen, 300, 95, 5, 5);
+            g.DrawEllipse(Whitepen, 60, 40, 5, 5);
+
 
         }
 
     }
+
+
 }
